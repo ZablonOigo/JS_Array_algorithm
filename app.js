1d562f599b23f1fd9fadae7f0e503df7f732eeab
Zablon Oigo <imjunior254@gmail.com>
2023-02-10 20:17:38 +0300

    test on validation

--- app.js
+++ app.js
@@ -13,4 +13,37 @@ class Person extends Employee{
 let person1=[new Employee(),new Person()];
 person1.forEach((msg)=>{
     msg.display();
-})
+});
+
+
+
+//Form validation
+
+
+const form=document.getElementById('form');
+const email=document.getElementById('email');
+
+const username=document.getElementById('username');
+const password=document.getElementById('password');
+const password2=document.getElementById('password2');
+form.addEventListener('submit',(e)=>{
+e.preventDefault();
+checkInput();
+});
+function checkInput(){
+    //get the values from inputs
+    const usernamevalue=username.value.trim();
+    const emailvalue=email.value.trim();
+    const passwordvalue=password.value.trim();
+    constpass2value= password2.vallue.trim();
+
+
+    //test condition
+    if(usernamevalue === ' '){
+        //show error
+        Seterrorfor(username,'usename cannot be blank')
+    }else{
+        //show valid
+        setsuccess(username);
+    }
+}
